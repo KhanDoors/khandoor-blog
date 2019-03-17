900a5bea5cb4c5782fc16f8af5f8dfb9f6e22ce3
unknown <syyadkhan@yahoo.com>
2019-03-16 20:31:40 -0400

    slug made

---
+++ src/templates/post-template.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const postTemplate = () => (
+  <Layout>
+    <h2>Programmatically created blog post</h2>
+  </Layout>
+)
+
+export default postTemplate
